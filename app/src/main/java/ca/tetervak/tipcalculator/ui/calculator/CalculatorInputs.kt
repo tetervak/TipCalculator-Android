3f568aa8f486df53ecef23a685bb7038b913dc17
Alex Tetervak <tetervak@sheridancollege.ca>
2023-09-19 21:02:47 -0400

    added preview functions

--- app/src/main/java/ca/tetervak/tipcalculator/ui/calculator/CalculatorInputs.kt
+++ app/src/main/java/ca/tetervak/tipcalculator/ui/calculator/CalculatorInputs.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import ca.tetervak.tipcalculator.R
@@ -129,5 +130,18 @@ fun ServiceCostInput(serviceCost: String, onChange: (String) -> Unit) {
         modifier = Modifier
             .padding(16.dp)
             .sizeIn(minWidth = 256.dp)
+    )
+}
+
+@Preview
+@Composable
+fun CalculatorInputsPreview(){
+    CalculatorInputs(
+        roundUpTip = true,
+        onChangeOfRoundUpTip = {},
+        serviceCost = "100",
+        onChangeOfServiceCost = {},
+        serviceQuality = ServiceQuality.GOOD,
+        onChangeOfServiceQuality = {}
     )
 }
--- app/src/main/java/ca/tetervak/tipcalculator/ui/calculator/CalculatorOutputs.kt
+++ app/src/main/java/ca/tetervak/tipcalculator/ui/calculator/CalculatorOutputs.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import ca.tetervak.tipcalculator.R
@@ -66,4 +67,10 @@ fun CalculatorOutputs(tipAmount: Double, billTotal: Double) {
 
 fun formatCurrency(amount: Double): String =
     NumberFormat.getCurrencyInstance().format(amount)
+
+@Preview
+@Composable
+fun CalculatorOutputsPreview(){
+    CalculatorOutputs(tipAmount = 18.0, billTotal = 118.0)
+}
 
