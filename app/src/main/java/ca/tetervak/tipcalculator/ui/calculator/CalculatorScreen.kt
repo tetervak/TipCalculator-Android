7c47c9e9d23912a67cbc243c404481518c4fbf11
Alex Tetervak <tetervak@sheridancollege.ca>
2023-09-27 12:10:53 -0400

    added scrolling

--- app/src/main/java/ca/tetervak/tipcalculator/ui/calculator/CalculatorScreen.kt
+++ app/src/main/java/ca/tetervak/tipcalculator/ui/calculator/CalculatorScreen.kt
@@ -1,9 +1,12 @@
 package ca.tetervak.tipcalculator.ui.calculator
 
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -24,7 +27,10 @@ fun CalculatorScreen(viewModel: CalculatorViewModel) {
     val roundUpTip: Boolean by viewModel.roundUpTip
 
     Column(
-        modifier = Modifier.padding(32.dp).fillMaxWidth(),
+        modifier = Modifier
+            .verticalScroll(rememberScrollState())
+            .padding(32.dp)
+            .fillMaxWidth(),
         verticalArrangement = Arrangement.spacedBy(32.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
