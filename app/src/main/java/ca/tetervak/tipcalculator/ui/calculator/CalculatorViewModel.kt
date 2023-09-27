fd6a2fea1c167471bcd9ff3710dfeaac93958092
Alex Tetervak <tetervak@sheridancollege.ca>
2023-09-27 09:44:33 -0400

    removed unused imports

--- app/src/main/java/ca/tetervak/tipcalculator/ui/calculator/CalculatorViewModel.kt
+++ app/src/main/java/ca/tetervak/tipcalculator/ui/calculator/CalculatorViewModel.kt
@@ -1,7 +1,5 @@
 package ca.tetervak.tipcalculator.ui.calculator
 
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import ca.tetervak.tipcalculator.domain.ServiceQuality
 import ca.tetervak.tipcalculator.domain.calculateTip
