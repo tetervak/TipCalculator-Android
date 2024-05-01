d298e358f788be8c37727f776250c77d6db58270
Alex Tetervak <tetervak@sheridancollege.ca>
2024-05-01 11:11:46 -0400

    removed unused imports

--- app/src/main/java/ca/tetervak/tipcalculator/ui/calculator/CalculatorBody.kt
+++ app/src/main/java/ca/tetervak/tipcalculator/ui/calculator/CalculatorBody.kt
@@ -6,19 +6,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import ca.tetervak.tipcalculator.R
-import ca.tetervak.tipcalculator.domain.ServiceQuality
 
 @Composable
 fun CalculatorBody(
