c5d1098df0fe41bc23772b285e6177e1bb6909a5
Alex Tetervak <tetervak@sheridancollege.ca>
2024-05-04 06:09:21 -0400

    removed saveable

--- app/src/main/java/ca/tetervak/tipcalculator/ui/AppRootScreen.kt
+++ app/src/main/java/ca/tetervak/tipcalculator/ui/AppRootScreen.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
@@ -24,7 +24,7 @@ import ca.tetervak.tipcalculator.ui.calculator.CalculatorTopBar
 @Composable
 fun AppRootScreen() {
 
-    var showAboutDialog: MutableState<Boolean> = rememberSaveable {
+    val showAboutDialog: MutableState<Boolean> = remember {
         mutableStateOf(false)
     }
 
