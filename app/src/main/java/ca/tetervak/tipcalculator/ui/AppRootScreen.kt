d75affcbacd0238ec02a7787ac3226ef3a0bc637
Alex Tetervak <tetervak@sheridancollege.ca>
2024-05-03 21:13:54 -0400

    added the about dialog

--- app/src/main/java/ca/tetervak/tipcalculator/ui/AppRootScreen.kt
+++ app/src/main/java/ca/tetervak/tipcalculator/ui/AppRootScreen.kt
@@ -2,23 +2,68 @@ package ca.tetervak.tipcalculator.ui
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.sp
+import ca.tetervak.tipcalculator.R
 import ca.tetervak.tipcalculator.ui.calculator.CalculatorBody
 import ca.tetervak.tipcalculator.ui.calculator.CalculatorTopBar
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AppRootScreen(){
+fun AppRootScreen() {
+
+    var showAboutDialog: MutableState<Boolean> = rememberSaveable {
+        mutableStateOf(false)
+    }
+
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
     Scaffold(
-        topBar = { CalculatorTopBar(scrollBehavior) },
-        modifier = Modifier.fillMaxSize().nestedScroll(scrollBehavior.nestedScrollConnection)
+        topBar = {
+            CalculatorTopBar(
+                onHelpButtonClick = { showAboutDialog.value = true },
+                scrollBehavior = scrollBehavior
+            )
+        },
+        modifier = Modifier
+            .fillMaxSize()
+            .nestedScroll(scrollBehavior.nestedScrollConnection)
     ) { innerPadding ->
         CalculatorBody(modifier = Modifier.padding(innerPadding))
     }
+
+    if (showAboutDialog.value) {
+        CalculatorAbout(onDismissRequest = { showAboutDialog.value = false })
+    }
 }
+
+@Composable
+fun CalculatorAbout(onDismissRequest: () -> Unit) =
+    AlertDialog(
+        onDismissRequest = onDismissRequest,
+        title = { Text(stringResource(R.string.about_calculator)) },
+        text = {
+            Text(
+                text = stringResource(R.string.programming_example),
+                fontSize = 18.sp
+            )
+        },
+        confirmButton = {
+            TextButton(
+                onClick = onDismissRequest
+            ) {
+                Text(stringResource(R.string.ok))
+            }
+        }
+    )
