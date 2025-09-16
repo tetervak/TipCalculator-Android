11a9a02d4d9c63a165fd5da6d52bdc5bca685bfc
Alexander Tetervak <tetervak@sheridancollege.ca>
2025-09-16 13:15:58 -0400

    updated libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "ca.tetervak.tipcalculator"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "ca.tetervak.tipcalculator"
         minSdk = 30
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
